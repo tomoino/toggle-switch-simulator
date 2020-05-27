9cde48a6b8e8825da4f77b9b51275d8d3d6ef1be
Tomohiro Inoue <55827264+tomoino@users.noreply.github.com>
2020-05-27 10:23:14 +0900

    [update]index
    u,v-tグラフ用の計算プログラムを追加。

--- pages/index.vue
+++ pages/index.vue
@@ -20,7 +20,32 @@ import Graph from '~/components/Graph.vue'
 export default {
   components: {
     Graph
-  }
+  },
+  methods: {
+    calcConcentration ( u0, v0, I1, I2) {
+      const [t0, tmax, division] = [0, 30, 100]
+      let U = [], V = [], T = []
+      let [u, v, t] = [u0, v0, t0]
+
+      const [a, n, I1] = [1, 8, 1]
+      const [b, m, I2] = [1, 8, 1]
+
+      let dudt = (_u, _v) =>  (a / (1 + Math.pow(_v, n))) - _u + I1
+      let dvdt = (_u, _v) =>  (b / (1 + Math.pow(_u, m))) - _v + I2
+      const dt = (tmax-t0) / division
+      
+      for ( let i = 0; i < division; i++) {
+        u += dt * dudt(u, v)
+        v += dt * dvdt(u, v)
+        t += dt
+        U.push(u)
+        V.push(v)
+        T.push(t)
+      }
+
+      return U,V,T; 
+    }
+  },
 }
 </script>
 
