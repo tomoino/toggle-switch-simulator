e49a52df519b487cc2637b4b42d59a1dfea1585f
Tomohiro Inoue <55827264+tomoino@users.noreply.github.com>
2020-06-26 18:52:51 +0900

    [fix]parameter bar
    スマホ版で左右にコンテンツがはみ出す不具合を修正。パラメタの調節バーのdivのmarginが誤って左右にかかっていたので上下に修正。

--- components/Graph.vue
+++ components/Graph.vue
@@ -1,6 +1,6 @@
 <template>
-    <div style="margin-bottom: 50px;">
-        <line-chart style="width: 95%;" :chart-data="datacollection" :options="values.options"></line-chart>
+    <div>
+        <line-chart :chart-data="datacollection" :options="values.options"></line-chart>
     </div>
 </template>
  
@@ -31,60 +31,7 @@ export default {
           labels: this.values.x,
           datasets: this.values.y
         }
-        // this.datacollection = {
-        //   labels: [this.getRandomInt(), this.getRandomInt()],
-        //   datasets: [
-        //     {
-        //       label: 'Test Data2',
-        //       backgroundColor: 'rgba(255, 100, 130, 0.2)',
-        //       data: [this.getRandomInt(), this.getRandomInt(), this.getRandomInt()]
-        //     }, {
-        //       label: 'Test Data2',
-        //       backgroundColor: 'rgba(100, 130, 255, 0.2)',
-        //       data: [this.getRandomInt(), this.getRandomInt(), this.getRandomInt()]
-        //     }
-        //     , {
-        //       label: 'Test Data3',
-        //       backgroundColor: 'rgba(130, 255, 100, 0.2)',
-        //       data: [this.getRandomInt(), this.getRandomInt(), this.getRandomInt()]
-        //     }
-        //   ]
-        // }
       }
     }
 };
-// import { ChartData, ChartOptions } from "chart.js";
-// import ChartLine from "~/components/ChartLine.vue";
-// import ChartBar from "~/components/ChartBar.vue";
-
-// export default {
-//   components: {
-//     ChartLine,
-//     ChartBar
-//   },
-//   props: {
-//     values: {
-//       type: Object,
-//       default: null
-//     }
-//   },
-//   data: () => {
-//     return {
-//         chartOptions: {
-//             maintainAspectRatio: false
-//         },
-//         chartStyles: {
-//             height: "100%",
-//             width: "100%"
-//         }
-//     }
-//   }
-// }
 </script>
-
-<style>
-  .small {
-    max-width: 600px;
-    margin:  150px auto;
-  }
-</style>
